2ac261a364a823a2b7541fa04b59d216e53cbb44
garbbagee <romandiazmiguelignacio@gmail.com>
2024-12-18 21:30:04 -0300

    first-first commit

--- ProyectoCompuStock/CompuStockApp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),  # Página principal, que usa la vista 'index'
-]
--- ProyectoCompuStock/CompuStockApp/views.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from django.shortcuts import render, get_object_or_404
-
-def index(request):
-    return render(request, 'html/index.html')

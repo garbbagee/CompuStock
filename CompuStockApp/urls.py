bd1b3c10475faedbbdf66e1827b244bee7f9d0b1
garbbagee <romandiazmiguelignacio@gmail.com>
2024-12-22 18:33:40 -0300

    front login, redirección y navbar listo.

--- CompuStockApp/urls.py
+++ CompuStockApp/urls.py
@@ -2,5 +2,26 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),  # Página principal, que usa la vista 'index'
+    # Página principal
+    path('', views.index, name='index'),
+    
+    # Quiénes somos
+    path('nosotros/', views.nosotros, name='nosotros'),
+    
+    # Login (Acceso a la cuenta)
+    path('login/', views.login_view, name='login'),
+    
+    # Registro de nuevo usuario
+    path('registro/', views.register_view, name='register'),
+    
+    # Categorías
+    path('hardware/', views.hardware, name='hardware'),
+    path('perifericos/', views.perifericos, name='perifericos'),
+    path('accesorios/', views.accesorios, name='accesorios'),
+    
+    # Ofertas
+    path('ofertas/', views.ofertas, name='ofertas'),
+    
+    # Soporte
+    path('soporte/', views.soporte, name='soporte'),
 ]
--- CompuStockApp/views.py
+++ CompuStockApp/views.py
@@ -1,5 +1,37 @@
-from django.urls import path
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 
+# Vista para la página principal
 def index(request):
     return render(request, 'html/index.html')
+
+# Vista para la página "Quiénes somos"
+def nosotros(request):
+    return render(request, 'html/nosotros.html')
+
+# Vista para el login
+def login_view(request):
+    return render(request, 'html/login.html')
+
+# Vista para el registro de un nuevo usuario
+def register_view(request):
+    return render(request, 'html/registro.html')
+
+# Vista para la categoría de Hardware
+def hardware(request):
+    return render(request, 'html/hardware.html')
+
+# Vista para la categoría de Periféricos
+def perifericos(request):
+    return render(request, 'html/perifericos.html')
+
+# Vista para la categoría de Accesorios
+def accesorios(request):
+    return render(request, 'html/accesorios.html')
+
+# Vista para las ofertas
+def ofertas(request):
+    return render(request, 'html/ofertas.html')
+
+# Vista para el soporte
+def soporte(request):
+    return render(request, 'html/soporte.html')
